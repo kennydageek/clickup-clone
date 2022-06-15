92d84981a43ed770188d26ddcfb4f0aae3dbd743
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-06-15 18:36:12 +0100

    Hamburger functionality implemented with JS

---
+++ script.js
@@ -0,0 +1,20 @@
+/*****************
+NAVIGATION SCRIPTS
+*****************/
+const overlay = document.querySelector('.nav-dropdown-overlay');
+const menuBtn = document.querySelector('.nav-icon__icon');
+const mobileNav = document.querySelector('.nav-dropdown');
+const cancel = document.querySelector('.nav-dropdown-cancel__icon');
+
+const showMenu = function () {
+    mobileNav.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeMenu = function () {
+    mobileNav.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+menuBtn.addEventListener('click', showMenu);
+cancel.addEventListener('click', closeMenu);
