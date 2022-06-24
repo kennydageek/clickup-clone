016d979438db8a5528a7ad74e7bc547fc31dc0c6
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-06-24 15:30:15 +0100

    section-support mobile

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ const mobileNav = document.querySelector('.nav-dropdown');
 const cancel = document.querySelector('.nav-dropdown-cancel__icon');
 
 const showMenu = function () {
-    mobileNav.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-}
+  mobileNav.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
 const closeMenu = function () {
-    mobileNav.classList.add('hidden');
-    overlay.classList.add('hidden');
-}
+  mobileNav.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
 menuBtn.addEventListener('click', showMenu);
 cancel.addEventListener('click', closeMenu);
